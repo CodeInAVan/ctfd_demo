60141b10254251c77c4256cc9e2b95151af2fcdb
Matthew Rawles <matthew@rawles.co.uk>
2022-10-06 22:20:41 +0100

    SSL configuration

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -9,7 +9,7 @@ domains=(cyberchallenges.uk www.cyberchallenges.uk)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="matthew@rawles.co.uk" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
